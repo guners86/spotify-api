5e8216b30618646c78b4ac6763d13b467b4f3923
guners86 <guners86@hotmail.com>
2015-10-26 16:40:50 -0500

    Se agrega funcionalidad de busqueda

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {	
+	$("#search-text").on('focus', function(e) {
+		e.preventDefault(); 
+		$(this).keydown( function(event) {
+			if (event.which === 13) {				
+				searchAlbums($("#search-text").val());
+			}
+		});		
+	});
 	
+	
+	var searchAlbums = function (query) {
+		alert(query);
+		$.ajax({
+			url: 'https://api.spotify.com/v1/search',
+			data: {
+				q: query,
+				type: 'track'
+			},
+			success: function (response) {
+				alert(response);
+				console.log(response);
+			},
+			error : function(xhr, status) {
+				alert('Disculpe, existió un problema');
+			}
+		});
+	};
 });
